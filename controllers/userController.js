a35bb577daaeba0156f89df65c605366af2b164e
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-03-14 21:58:11 -0300

    Integrate server with database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const db = require('../db');
 
 const openRouter = express.Router();
 
 
 openRouter.get('/', async (req, res) => {
-    res.send({user: 'luiz'});
+    const { rows } = await db.query("SELECT * FROM usermodel WHERE name = 'Luiz'");
+    console.log(rows);
+    res.send({user: rows[0]});
 });
 
 module.exports = app => app.use('/user', openRouter);
